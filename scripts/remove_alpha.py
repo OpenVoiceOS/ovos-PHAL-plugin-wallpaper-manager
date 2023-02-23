eb63def2f9f7021a820cf46aa4aebc320fdec700
Aditya Mehra <aix.m@outlook.com>
2023-02-24 09:16:10 +1030

    fix scripts

--- scripts/remove_alpha.py
+++ scripts/remove_alpha.py
@@ -2,7 +2,7 @@ import fileinput
 from os.path import join, dirname
 
 
-version_file = join(dirname(dirname(__file__)), "ovos_PHAL_plugin_wifi_setup", "version.py")
+version_file = join(dirname(dirname(__file__)), "ovos_PHAL_plugin_wallpaper_manager", "version.py")
 
 alpha_var_name = "VERSION_ALPHA"
 
