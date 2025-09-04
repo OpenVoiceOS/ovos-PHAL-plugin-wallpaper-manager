d2339082d347d844f01f71d92c404726fff7f5f1
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2025-09-04 12:44:42 -0700

    Refactor wallpaper_rotation init (#33)
    * Refactor wallpaper_rotation init
    
    * Refactor to activate wallpaper rotation on `mycroft.ready`

--- ovos_PHAL_plugin_wallpaper_manager/__init__.py
+++ ovos_PHAL_plugin_wallpaper_manager/__init__.py
@@ -53,10 +53,7 @@ class WallpaperManager(PHALPlugin):
         self.bus.on("ovos.wallpaper.manager.get.auto.rotation", self.handle_get_auto_rotation)
 
         self.bus.on("homescreen.metadata.get", self.handle_homescreen_load)
-
-        if self.wallpaper_rotation:
-            # Start rotation if configured
-            self._start_auto_rotation()
+        self.bus.on("mycroft.ready", self.handle_ready)
 
         # We cannot guarantee when this plugin will be loaded, so emit a message
         # to any providers that are waiting for the plugin to be loaded, so they
@@ -66,6 +63,11 @@ class WallpaperManager(PHALPlugin):
     def handle_homescreen_load(self, message: Message):
         self.bus.emit(Message("homescreen.wallpaper.set",
                               {"url": self.selected_wallpaper}))
+
+    def handle_ready(self, message: Message):
+        # Start rotation if configured
+        if self.wallpaper_rotation:
+            self._start_auto_rotation()
 
     def populate_wallpapers(self):
         LOG.info(f"default wallpapers storage: {self.local_wallpaper_storage}")
